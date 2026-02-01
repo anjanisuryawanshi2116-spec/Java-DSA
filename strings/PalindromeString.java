fdd93d9a6cd821c05ac154d5f55328ac2bb0ad86
Anjani Suryawanshi <anjanisuryawanshi2116@gmail.com>
2026-02-01 18:29:59 +0530

    Add string palindrome problems

---
+++ strings/PalindromeString.java
@@ -0,0 +1,17 @@
+public class PalindromeString {
+    public static void main(String[] args) {
+        String str = "madam";
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        if (str.equals(reversed)) {
+            System.out.println("Palindrome String");
+        } else {
+            System.out.println("Not a Palindrome String");
+        }
+    }
+}
+
